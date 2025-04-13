83ee8b5badb97121f1ba23c7dc1764431076b39c
Vijay Sundaram Mohana <vijaysundaram0206@gmail.com>
2025-04-13 18:18:01 +0100

    Update app.py
    - updated headers in viz

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Set page config
-st.set_page_config(page_title="IPL Impact Score Viewer", layout="centered")
+st.set_page_config(page_title="IPL Impact Score", layout="centered")
 
 # Load the three pickle files
 with open("is_player_output.pkl", "rb") as file:
@@ -28,9 +28,12 @@ st.title("IPL Impact Score")
 match_id = st.number_input("Enter Match ID", min_value=0, step=1)
 st.info("Open the specific match on ESPNcricinfo and copy the long number at the end of the URL.")
 
-# Impact type selector
-impact_type = st.selectbox("Select Impact Score Type", ["Player-wise", "Over-wise", "Phase-wise"])
-
+# Impact type selector as tiles
+impact_type = st.radio(
+    "Select Impact Score Type",
+    ["Player-wise", "Over-wise", "Phase-wise"],
+    horizontal=True
+)
 
 # Proceed if match ID is entered
 if match_id:
@@ -50,11 +53,11 @@ if match_id:
         df_filtered = impact_overwise[impact_overwise["p_match"] == match_id]
 
         if not df_filtered.empty:
-            st.subheader("Over-by-Over Batting Impact Score by Team")
+            st.subheader("Over-wise Impact Score")
             fig, ax = plt.subplots(figsize=(14, 6))
             sns.barplot(data=df_filtered, x='over', y='impact_bat', hue='team_bat', ax=ax)
 
-            ax.set_title('Over-by-Over Batting Impact Score by Team')
+            ax.set_title('Over-wise Impact Score')
             ax.set_xlabel('Over')
             ax.set_ylabel('Impact Score')
             ax.set_xticks(range(0, 20))
@@ -63,17 +66,17 @@ if match_id:
             ax.grid(axis='y', linestyle='--', alpha=0.6)
             st.pyplot(fig)
         else:
-            st.warning("No over-wise data found for the entered Match ID.")
+            st.warning("No data found for the entered Match ID.")
 
     elif impact_type == "Phase-wise":
         df_filtered = impact_phasewise[impact_phasewise["p_match"] == match_id]
 
         if not df_filtered.empty:
-            st.subheader("Phase-wise Batting Impact Score by Team")
+            st.subheader("Phase-wise Impact Score")
             fig, ax = plt.subplots(figsize=(10, 6))
             sns.barplot(data=df_filtered, x='phase', y='impact_bat', hue='team_bat', ax=ax)
 
-            ax.set_title('Phase-wise Batting Impact Score by Team')
+            ax.set_title('Phase-wise Impact Score')
             ax.set_xlabel('Match Phase')
             ax.set_ylabel('Impact Score')
             ax.grid(axis='y', linestyle='--', alpha=0.6)
